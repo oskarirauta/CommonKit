f4cb551ab81ff9ed6aa8bad688a379fafedbdc63
oskarirauta <oskari.rauta@gmail.com>
2018-05-25 00:56:25 +0300

    String conversion extension
    Character as String

---
+++ CommonKit/CommonKit/Extensions/Character.swift
@@ -0,0 +1,17 @@
+//
+//  Character.swift
+//  CommonKit
+//
+//  Created by Oskari Rauta on 21/05/2018.
+//  Copyright © 2018 Oskari Rauta. All rights reserved.
+//
+
+import Foundation
+
+extension Character {
+    
+    public var string: String {
+        get { return String(self) }
+    }
+    
+}
