06ec74519f486cd5cda45c3f733b2dd009cb03d8
oskarirauta <oskari.rauta@gmail.com>
2018-06-08 13:42:42 +0300

    Fix warnSyntaxError
    Changes to UIColor requires minor changes..

--- CommonKit/CommonKit/Extensions/UITableViewCell.swift
+++ CommonKit/CommonKit/Extensions/UITableViewCell.swift
@@ -13,7 +13,7 @@ extension UITableViewCell {
 
     public func warnSyntaxError() {
         let origColor: CGColor? = self.layer.backgroundColor
-        self.layer.backgroundColor = UIColor.red.lighter(0.7).cgColor
+        self.layer.backgroundColor = UIColor.red.lighter(by: 0.7).cgColor
         
         UIView.animate(withDuration: TimeInterval(0.68), animations: {
             self.layer.backgroundColor = origColor
