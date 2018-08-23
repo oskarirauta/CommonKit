d8f32000b2b9feabe650a6de809169a854287ab5
oskarirauta <oskari.rauta@gmail.com>
2018-08-23 12:25:25 +0300

    Add ConditionalInit
    Add ConditionalInit

---
+++ CommonKit/CommonKit/Extensions/ConditionalInit.swift
@@ -0,0 +1,22 @@
+//
+//  AutoCreatable.swift
+//  CommonKit
+//
+//  Created by Oskari Rauta on 23/08/2018.
+//
+
+import Foundation
+
+public protocol ConditionalInit {
+    mutating func conditionalInit(_ creatorFunc: ((Self) -> Void)?) -> Self
+}
+
+extension Optional:ConditionalInit where Wrapped: NSObject {
+    
+    public mutating func conditionalInit(_ creatorFunc: ((Optional<Wrapped>) -> Void)? = nil) -> Optional<Wrapped> {
+        guard self == nil else { return self! }
+        self = Wrapped.init()
+        creatorFunc?(self!)
+        return self!
+    }
+}
