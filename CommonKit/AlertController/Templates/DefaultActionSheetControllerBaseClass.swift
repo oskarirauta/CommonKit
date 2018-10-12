d665e5835482ccf030a39750ef9c84bd6340dc20
oskarirauta <oskari.rauta@gmail.com>
2018-10-12 10:56:29 +0300

    Release 1.6.5
    Adds template for ActionSheetController

---
+++ CommonKit/AlertController/Templates/DefaultActionSheetControllerBaseClass.swift
@@ -0,0 +1,14 @@
+//
+//  DefaultActionSheetControllerBaseClass.swift
+//  CommonKit
+//
+//  Created by Oskari Rauta on 12/10/2018.
+//  Copyright © 2018 Oskari Rauta. All rights reserved.
+//
+
+import Foundation
+
+open class DefaultActionSheetControllerBaseClass: DefaultAlertControllerBaseClass {
+    
+    open override var fullscreen: Bool { get { return false } }
+}
