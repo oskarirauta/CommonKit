df0ac59130fdd3e5a95b9f014682f77694b20a30
oskarirauta <oskari.rauta@gmail.com>
2018-11-01 01:16:06 +0200

    More improvements to UITextField
    Easy cursor position management.

--- CommonKit/CommonKit/Extensions/UITextField.swift
+++ CommonKit/CommonKit/Extensions/UITextField.swift
@@ -11,6 +11,25 @@ import UIKit
 
 extension UITextField {
     
+    public var cursorPosition: Int? {
+        get {
+            guard let selectedRange: UITextRange = self.selectedTextRange else { return nil }
+            return self.positionToInt(from: selectedRange.start)
+        }
+        set {
+            guard
+                newValue != nil,
+                let pos: UITextPosition = self.position(from: self.beginningOfDocument, offset: newValue!),
+                let range: UITextRange = self.textRange(from: pos, to: pos)
+                else { return }
+            self.selectedTextRange = range
+        }
+    }
+
+    public func positionToInt(from position: UITextPosition) -> Int {
+        return self.offset(from: self.beginningOfDocument, to: position)
+    }
+    
     public func warnSyntaxError() {
         let origColor: CGColor? = self.layer.backgroundColor
         self.layer.backgroundColor = UIColor.red.lighter(by: 0.7).cgColor
