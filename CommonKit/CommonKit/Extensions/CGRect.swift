3ec6cda5632d5faf95c8b8fd195640bd1936d473
oskarirauta <oskari.rauta@gmail.com>
2018-11-08 15:37:33 +0200

    Release 1.6.27
    Improvements to previous extensions.

--- CommonKit/CommonKit/Extensions/CGRect.swift
+++ CommonKit/CommonKit/Extensions/CGRect.swift
@@ -11,11 +11,11 @@ import Foundation
 extension CGRect {
     
     public func grow(by insets: UIEdgeInsets) -> CGRect {
-        return CGRect(x: self.origin.x - insets.left, y: self.origin.y - insets.top, width: self.size.width + insets.right, height: self.size.height + insets.bottom)
+        return CGRect(x: self.origin.x - insets.left, y: self.origin.y - insets.top, width: self.size.width + ( insets.right * 2.0 ), height: self.size.height + ( insets.bottom * 2.0 ))
     }
 
     public func grow(by size: CGSize) -> CGRect {
-        return CGRect(x: self.origin.x - size.width, y: self.origin.y - size.height, width: self.size.width + size.width, height: self.size.height + size.height)
+        return CGRect(x: self.origin.x - size.width, y: self.origin.y - size.height, width: self.size.width + ( size.width * 2.0 ), height: self.size.height + ( size.height * 2.0 ))
     }
     
     public func grow(width: Double, height: Double) -> CGRect {
