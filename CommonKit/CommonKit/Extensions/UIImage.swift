b48b19e1e5dffed19b59cade313eebaa123733cf
oskarirauta <oskari.rauta@gmail.com>
2018-12-05 13:51:49 +0200

    Release 1.7.4

--- CommonKit/CommonKit/Extensions/UIImage.swift
+++ CommonKit/CommonKit/Extensions/UIImage.swift
@@ -185,3 +185,15 @@ extension UIImage {
     }
     
 }
+
+extension UIImage {
+
+    public func image(alpha: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: .zero, blendMode: .normal, alpha: alpha)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
+}
